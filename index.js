d38dd1ec35b646b394a11695cb34f7cff9b16acb
marcell2204 <gyumar130@hengersor.hu>
2025-09-11 11:47:48 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mysql = require("mysql");
+app.use(cors());              
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "127.0.0.1",
+    port: 3306,
+    password: "root",
+    database:""
+}); 
+
+app.get("/", (req, res) => {
+    res.send("Fut a backend!");
+})
+app.listen(3001, () => {
+    console.log("Server is running on port 3001");
+});
