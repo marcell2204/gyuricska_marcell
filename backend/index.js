e79532231c58e850fe8c916e5bc635f2a2660b98
marcell2204 <gyumar130@hengersor.hu>
2025-09-11 12:49:05 +0200

    .

---
+++ backend/index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mysql = require("mysql");
+app.use(cors());              
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "127.0.0.1",
+    port: 3307,
+    database:"kozutak"
+}); 
+
+app.get("/", (req, res) => {
+    res.send("Fut a backend!");
+})
+app.listen(3001, () => {
+    console.log("Server is running on port 3001");
+});
+
+app.get("/regiok", (req, res) => {
+    const sql = "SELECT * FROM `regiok`";
+    db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result)
+    })
+})  
+
+app.get("/gregiok", (req, res) => {
+    const sql = "SELECT * FROM `regiok` WHERE regionev LIKE '%g%'";
+    db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result)
+    })
+})  
+app.get("/sorrend", (req, res) => {
+    const sql = "SELECT regionev FROM `regiok` GROUP BY regionev DESC";
+    db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result)
+    })
+})  
+
+app.get("/hossz", (req, res) => {
+    const sql = "SELECT MAX(hossz) FROM `kozutak_hossza`";
+    db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result)
+    })
+})  
